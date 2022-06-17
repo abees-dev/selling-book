3ca2165ef582a0e057669911689fdfc0c56fe04e
Abeess <kurotoxic2510@gmal.com>
2022-06-18 04:15:47 +0700

    make it better

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
-import bodyParser from 'body-parser';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import connectDB from './config/connectDB';
-import router from './router/router';
-import cors from 'cors';
-import 'dotenv/config';
+import bodyParser from "body-parser";
+import express from "express";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/connectDB";
+import router from "./router/router";
+import cors from "cors";
+import "dotenv/config";
 
 connectDB();
 const app = express();
@@ -14,6 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
-app.use('/', router);
-const PORT = process.env.PORT || 8080;
+app.use("/", router);
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
